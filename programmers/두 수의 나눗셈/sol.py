35f3de24b4e5f7746f5afd4d8344ccd11aca287a
seunghyun <tmdgus_skfk@naver.com>
2025-04-07 17:17:36 +0900

    update

---
+++ programmers/두 수의 나눗셈/sol.py
@@ -0,0 +1,7 @@
+def solution(num1, num2):
+    result = (num1 / num2) * 1000
+    return int(result)
+
+print(solution(3, 2))
+print(solution(7, 3))
+print(solution(1, 16))
---
+++ programmers/두 수의 합 구하기/sol.py
@@ -0,0 +1,7 @@
+def solution(num1, num2):
+    result = (num1 + num2)
+    
+    return int(result)
+
+print(solution(2, 3))
+print(solution(100, 2))
